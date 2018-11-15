940fd76023d8dd9ae490b66d784816920a7ecc2a
EaqueSH <paraghostsociety@gmail.com>
2018-11-15 16:19:28 +0100

    Install rushfollowers

---
+++ application/views/site/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>RushFollowers - Boostez votre notoriété</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-select.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/imagehover.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css.map">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.css.map">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.min.css.map">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+</head>
+<nav class="navbar navbar-light indexing col-xs-12">
+        <div class="row-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed " data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar colorFond"></span>
+        <span class="icon-bar colorFond"></span>
+        <span class="icon-bar colorFond"></span>
+      </button>
+                <a class="navbar-brand colortitle" href="#">Il était une fois</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav pull-right">
+                    <li><a class="navLinkOne" href="">Accueil</a></li>
+                    <li><a id="aboutScroll" class=" navLinkTwo">A propos</a></li>
+                    <li><a id="avantScroll" class=" navLinkTwo">Avant & Après</a></li>
+                    <li><a id="serviceScroll" class=" navLinkThree">Nos services</a></li>
+                    <li><a id="contactScroll" class=" navLinkFour">Contactez nous</a></li>
+                </ul>
+
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+<body>
+
+
+<div class="row contain">
+
+
+
+
+
+</div>
+    
+</body>
+</html>
